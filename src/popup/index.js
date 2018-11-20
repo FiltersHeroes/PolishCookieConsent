d57b1c971e3eeb385a98cdb6392544101d85076a
hawkeye116477 <hawkeye116477@gmail.com>
2018-11-20 16:25:00 +0100

    Move Internationalization to one script

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,3 +1,11 @@
+// ----------------- Internationalization ------------------
+for (let node of document.querySelectorAll('[data-i18n]')) {
+  let [text, attr] = node.dataset.i18n.split('|');
+  text = chrome.i18n.getMessage(text);
+  attr ? node[attr] = text : node.appendChild(document.createTextNode(text));
+}
+// ----------------- /Internationalization -----------------
+
 var wrapper = document.querySelectorAll(".wrapper");
 
 for (var i = 0; i < wrapper.length; i++) {
