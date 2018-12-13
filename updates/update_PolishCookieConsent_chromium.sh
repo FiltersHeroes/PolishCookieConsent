009b03ef26becdcbd91d09140a0b7db868bafb71
hawkeye116477 <hawkeye116477@gmail.com>
2018-12-13 20:44:36 +0100

    Update Linux script
    Fix path to home directory

--- updates/update_PolishCookieConsent_chromium.sh
+++ updates/update_PolishCookieConsent_chromium.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# v1.0.4
+# v1.0.5
 
 cat > /tmp/update_PolishCookieConsent_chromium << 'EOF'
 #!/bin/sh
+USER_HOME=$(getent passwd $SUDO_USER | grep home | cut -d: -f6)
+
 NEW_VERSION=$(curl --silent "https://api.github.com/repos/PolishFiltersTeam/PolishCookieConsent/releases/latest" | grep '"tag_name":'|sed -E 's/.*"([^"]+)".*/\1/' | awk '{gsub("v", "");print}')
 
-PATH_EXT=~/Rozszerzenia/PolishCookieConsent
+PATH_EXT=$USER_HOME/Rozszerzenia/PolishCookieConsent
 
 OLD_VERSION=$(cat $PATH_EXT/manifest.json | grep '"version":' | sed -E 's/.*"([^"]+)".*/\1/')
 
@@ -15,7 +17,7 @@ if (( $(echo "$NEW_VERSION $OLD_VERSION" | awk '{print ($1 > $2)}') )); then
     notify-send 'Powiadomienie o aktualizacji' 'Nowa wersja rozszerzenia Polska Ciasteczkowa Zgoda jest już dostępna! Pobieram nową wersję...'
     cd $(xdg-user-dir DOWNLOAD)
     wget https://github.com/PolishFiltersTeam/PolishCookieConsent/releases/download/v${NEW_VERSION}/PolishCookieConsent_chromium.zip -P ./PolishCookieConsent_v$NEW_VERSION/
-    unzip -o ./PolishCookieConsent_v$NEW_VERSION/PolishCookieConsent_chromium.zip -d ~/Rozszerzenia/PolishCookieConsent
+    unzip -o ./PolishCookieConsent_v$NEW_VERSION/PolishCookieConsent_chromium.zip -d $USER_HOME/Rozszerzenia/PolishCookieConsent
     rm -rf ./PolishCookieConsent_v$NEW_VERSION
     export DISPLAY=:0.0
     notify-send 'Sukces' 'Rozszerzenie Polska Ciasteczkowa Zgoda zostało zaktualizowane. Proszę zrestartować przeglądarkę.'
