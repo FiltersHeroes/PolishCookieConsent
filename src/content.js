0b9a2c5fce88f129d1d563f2aec02aca6c40e5f8
hawkeye116477 <hawkeye116477@gmail.com>
2018-12-23 19:57:21 +0100

    Accept policy of action.com and tidy up :smile:

--- src/content.js
+++ src/content.js
@@ -1,157 +1,132 @@
-function clickInteractive(element)
-{
-    var readyStateCheckInterval = setInterval(function() {
-        if (document.readyState === "interactive") {
-            var btnYes = document.querySelector(element);
-            if (btnYes)
-            {
-                btnYes.click();
-            }
-        }
-    }, 10);
-}
-
-function bakeCookie(cookieName, cookieValue, expiresDays)
-{
-    if (document.cookie.indexOf(cookieName+"=") == -1)
-    {
-        var d = new Date();
-        d.setTime(d.getTime() + (expiresDays*24*60*60*1000));
-        var expires = "expires="+ d.toUTCString();
-        document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
-        location.reload();
-    }
-}
-
-function addToStorage(storageElement, storageValue)
-{
-    if (localStorage.getItem(storageElement) === undefined || localStorage.getItem(storageElement) === null)
-    {
-        localStorage.setItem(storageElement, storageValue);
-    }
-}
-
 var url = location.href;
 
-if(url.match(/http:\/\/f1racing.pl/) || url.match(/http:\/\/www.f1racing.pl/))
+function getUrlCondition(urlArg)
 {
-    if(document.cookie.indexOf("x-id-cookie-yes=") == -1)
+    var condition;
+    if(urlArg.match("="))
     {
-        window.location = "/x-set-cookie" + location.pathname;
+        condition = url==urlArg.replace("=", "");
+    }
+    else
+    {
+        condition = url.match(RegExp(urlArg));
+    }
+    return condition;
+}
+
+function clickInteractive(element, urlArg)
+{
+    if(getUrlCondition(urlArg))
+    {
+        var readyStateCheckInterval = setInterval(function() {
+            if (document.readyState === "interactive") {
+                var btnYes = document.querySelector(element);
+                if (btnYes)
+                {
+                    btnYes.click();
+                }
+            }
+        }, 10);
     }
 }
 
-if(url.match(/tumblr.com\/privacy\/consent/))
+function clickComplete(element, urlArg, cookieName)
 {
-    clickInteractive('.btn.yes');
-}
-
-if(url.match(/sportowefakty.wp.pl/) || url.match(/wirtualnemedia.pl/))
-{
-    var readyStateCheckInterval = setInterval(function() {
-        if (document.readyState === "complete") {
-            var shadowHost = document.querySelectorAll('.twitter-tweet'), i;
-            var cookieConsent = document.querySelector('.twitter-tweet').shadowRoot.querySelector('.Interstitial');
-            if(cookieConsent)
-            {
-                for (i=0; i<shadowHost.length; ++i)
+    if(getUrlCondition(urlArg))
+    {
+        var readyStateCheckInterval = setInterval(function() {
+            if (document.readyState === "complete") {
+                var btnYes = document.querySelector(element);
+                if (document.cookie.indexOf(cookieName+"=") == -1)
                 {
-                    shadowHost[i].shadowRoot.querySelector('.Interstitial').remove();
+                    btnYes.click();
                 }
             }
-        }
-    }, 1000);
+        }, 10);
+    }
 }
 
-if(url.match(/gry.pl/))
+function bakeCookie(cookieName, cookieValue, expiresDays, urlArg)
 {
-    var readyStateCheckInterval = setInterval(function() {
-        if (document.readyState === "complete") {
-            var btnYes = document.body.querySelector('div[class*="app_gdpr"] button[class*="intro_acceptAll"]');
-            if (document.cookie.indexOf("euconsent=") == -1)
+    if(getUrlCondition(urlArg))
+    {
+        if (document.cookie.indexOf(cookieName+"=") == -1)
+        {
+            var d = new Date();
+            d.setTime(d.getTime() + (expiresDays*24*60*60*1000));
+            var expires = "expires="+ d.toUTCString();
+            document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+            location.reload();
+        }
+    }
+}
+
+function addToStorage(storageElement, storageValue, urlArg)
+{
+    if(getUrlCondition(urlArg))
+    {
+        if (localStorage.getItem(storageElement) === undefined || localStorage.getItem(storageElement) === null)
+        {
+            localStorage.setItem(storageElement, storageValue);
+        }
+    }
+}
+
+
+function redirect(redirectPoint, pathName, urlArg, cookieName)
+{
+    if(getUrlCondition(urlArg))
+    {
+        if(document.cookie.indexOf(cookieName) == -1)
+        {
+            if(pathName=="true")
             {
-                btnYes.click();
+                window.location = redirectPoint + location.pathname;
+            }
+            else
+            {
+                window.location = redirectPoint;
             }
         }
-    }, 10);
+    }
 }
 
-if(url.match(/vivaldi.com/))
+function removeFromShadow(shadowHostArg, element, urlArg)
 {
-    bakeCookie("acceptedCookies", "true", "365");
-}
-
-if(url.match(/guce.oath.com\/collectConsent/))
-{
-    clickInteractive('.btn.agree');
-}
-
-if(url.match(/unileverfoodsolutions.pl/))
-{
-    clickInteractive('.evidon-barrier-acceptbutton');
-}
-
-if(url.match(/downdetector.pl/))
-{
-    clickInteractive('#consentButton');
-}
-
-if(url.match(/hiszpanskidlapolakow.com/))
-{
-    var readyStateCheckInterval = setInterval(function() {
-        if (document.readyState === "interactive") {
-            var btnYes = document.querySelector("#gdpr-accept .btn");
-            if (btnYes)
-            {
-                document.getElementById("facebook").checked = true;
-                document.getElementById("disqus").checked = true;
-                btnYes.click();
+    if(getUrlCondition(urlArg))
+    {
+        var readyStateCheckInterval = setInterval(function() {
+            if (document.readyState === "complete") {
+                var shadowHost = document.querySelectorAll(shadowHostArg), i;
+                var cookieConsent = document.querySelector(shadowHostArg).shadowRoot.querySelector(element);
+                if(cookieConsent)
+                {
+                    for (i=0; i<shadowHost.length; ++i)
+                    {
+                        shadowHost[i].shadowRoot.querySelector(element).remove();
+                    }
+                }
             }
-        }
-    }, 10);
+        }, 1000);
+    }
 }
 
-if(url.match(/espedytor.pl/))
-{
-    bakeCookie("zgodaRODO", "true", "365");
-}
-
-if(url.match(/wikia.com/) || url.match(/fandom.com/))
-{
-    clickInteractive('[data-tracking-opt-in-accept="true"]');
-}
-
-if(url.match(/odr.pl/))
-{
-    clickInteractive('.termsagree');
-}
-
-if(url.match(/drogerium.pl/) || url.match(/wylecz.to/) || url.match(/budujmase.pl/))
-{
-    clickInteractive('#accept-targeting-disclaimer-button');
-}
-
-if(url.match(/pfm.pl/))
-{
-    clickInteractive('#NeucaCookieConsent .btn-primary');
-}
-
-if(url.match(/rodobox.io/) || url.match(/totalnareklama.pl/))
-{
-    clickInteractive('#_rdbxAcceptAllBtn');
-}
-
-if(url=="https://powiatkamienski.pl/")
-{
-    window.location = "https://powiatkamienski.pl/aktualnosci.dhtml";
-}
-
-if(url.match(/gry.lotto.pl/))
-{
-    addToStorage("rodoConfirmation", true);
-}
-
-if(url.match(/totalcasino.pl/))
-{
-    addToStorage("gdpr_popup", true);
-}
+clickInteractive('.btn.yes', 'tumblr.com\/privacy\/consent');
+clickInteractive('.btn[name="agree"]', 'guce.oath.com\/collectConsent');
+clickInteractive('.evidon-barrier-acceptbutton', 'unileverfoodsolutions.pl');
+clickInteractive('#consentButton', 'downdetector.pl');
+clickInteractive('[data-tracking-opt-in-accept="true"]', 'wikia.com|fandom.com');
+clickInteractive('.termsagree', 'odr.pl');
+clickInteractive('#accept-targeting-disclaimer-button', 'drogerium.pl|wylecz.to|budujmase.pl');
+clickInteractive('#NeucaCookieConsent .btn-primary', 'pfm.pl');
+clickInteractive('#_rdbxAcceptAllBtn', 'rodobox.io|totalnareklama.pl');
+bakeCookie("acceptedCookies", "true", "365", "vivaldi.com");
+bakeCookie("zgodaRODO", "true", "365", "espedytor.pl");
+bakeCookie('gdpr-accepted', '{"ga":true,"facebook":true,"disqus":true}', '365', 'hiszpanskidlapolakow.com');
+bakeCookie('CookieConsent', 'true', '365', 'action.com');
+addToStorage("rodoConfirmation", "true", "gry.lotto.pl");
+addToStorage("gdpr_popup", "true", "totalcasino.pl");
+redirect("/x-set-cookie", "true", "f1racing.pl", "x-id-cookie-yes=");
+redirect("/aktualnosci.dhtml", "", "=https://powiatkamienski.pl/");
+clickComplete('div[class*="app_gdpr"] button[class*="intro_acceptAll"]', "gry.pl", "euconsent");
+removeFromShadow(".twitter-tweet", ".Interstitial", "sportowefakty.wp.pl|wirtualnemedia.pl");
