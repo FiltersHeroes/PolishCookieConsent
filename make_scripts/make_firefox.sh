6bb8ab8d75f911b19b9e147d32d602703c8ff36d
hawkeye116477 <hawkeye116477@gmail.com>
2019-04-11 00:19:29 +0200

    Ulepszenie skryptu
    Aktualizacje są teraz w AMO, więc możemy zrezygnować z uploadu do GitHub Releases

--- make_scripts/make_firefox.sh
+++ make_scripts/make_firefox.sh
@@ -5,10 +5,4 @@ sciezka=$(dirname "$0")
 
 cd $sciezka/../src
 
-function npm-do { (PATH=$(npm bin):$PATH; eval $@;) }
-
-npm-do shipit firefox ./
-
-if [ -f ./web-ext-artifacts/polish_cookie_consent-*.xpi ]; then
-mv ./web-ext-artifacts/polish_cookie_consent-*.xpi ../PolishCookieConsent_firefox.xpi
-fi
+npx shipit firefox ./
