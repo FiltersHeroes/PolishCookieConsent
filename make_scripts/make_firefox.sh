7aa3c2b5b358ea79d7075e23f343f429704e6df6
hawkeye116477 <hawkeye116477@gmail.com>
2019-04-10 23:09:07 +0200

    Poprawa skryptu

--- make_scripts/make_firefox.sh
+++ make_scripts/make_firefox.sh
@@ -5,6 +5,8 @@ sciezka=$(dirname "$0")
 
 cd $sciezka/../src
 
+function npm-do { (PATH=$(npm bin):$PATH; eval $@;) }
+
 npm-do shipit firefox ./
 
 if [ -f ./web-ext-artifacts/polish_cookie_consent-*.xpi ]; then
