5361367009a66be3503b887bfeb9fae8d40621dc
hawkeye116477 <hawkeye116477@gmail.com>
2019-05-19 20:24:57 +0200

    Dodanie usuwania zbędnego info dla Chromium

--- make_scripts/make_chromium.sh
+++ make_scripts/make_chromium.sh
@@ -4,6 +4,9 @@
 sciezka=$(dirname "$0")
 
 cd $sciezka/../src
+jq 'del(.applications)' manifest.json > manifest.json.temp
+rm -r manifest.json
+mv manifest.json.temp manifest.json
 zip -r PolishCookieConsent_chromium.zip *
 mkdir ./web-ext-artifacts
 mv ./PolishCookieConsent_chromium.zip ./web-ext-artifacts
