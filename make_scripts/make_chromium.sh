bf9e4ee91a59d929d83c8748c96bcd83f1d86be1
hawkeye116477 <hawkeye116477@gmail.com>
2019-05-21 21:11:45 +0200

    Poprawka do skryptu Chromium

--- make_scripts/make_chromium.sh
+++ make_scripts/make_chromium.sh
@@ -7,6 +7,11 @@ cd $sciezka/../src
 jq 'del(.applications)' manifest.json > manifest.json.temp
 rm -r manifest.json
 mv manifest.json.temp manifest.json
+
+if [ -d $sciezka/../src/web-ext-artifacts ]; then
+    rm -rvf ./web-ext-artifacts;
+fi
+
 npx shipit chrome ./
 zip -r PolishCookieConsent_chromium.zip *
 mkdir ./web-ext-artifacts
