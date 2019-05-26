6f60daf573e90094cbae6752cb7bab63a09a51ab
hawkeye116477 <hawkeye116477@gmail.com>
2019-05-26 10:14:34 +0200

    Mała poprawka

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,5 +1,5 @@
 // ----------------- Internationalization ------------------
-for (let node of document.querySelectorAll('[data-i18n]')) {
+for (const node of document.querySelectorAll('[data-i18n]')) {
   let [text, attr] = node.dataset.i18n.split('|');
   text = chrome.i18n.getMessage(text);
   attr ? node[attr] = text : node.appendChild(document.createTextNode(text));
