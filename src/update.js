fa3c124a00db9a8835cb761a6f834238acbe2580
hawkeye116477 <hawkeye116477@gmail.com>
2021-04-11 20:25:35 +0200

    #72

--- src/update.js
+++ src/update.js
@@ -15,18 +15,20 @@ function handleTextResponse(response) {
         })
 }
 
-chrome.runtime.onInstalled.addListener(function () {
-    fetch('/PCB.txt')
-        .then(handleTextResponse)
-        .catch(error => console.log(error));
+chrome.runtime.onInstalled.addListener(function (details) {
+    if (details.reason === "install" || details.reason === "update") {
+        fetch('/PCB.txt')
+            .then(handleTextResponse)
+            .catch(error => console.log(error));
 
-    var _updateTime = new Date().getTime() + 24 * 7 * 60 * 60 * 1000;
-    chrome.storage.local.set({
-        updateTime: _updateTime
-    });
-    chrome.storage.local.get(['updateTime'], function (result) {
-        chrome.alarms.create('updateCookieBase', { when: parseInt(result.updateTime) });
-    });
+        var _updateTime = new Date().getTime() + 24 * 7 * 60 * 60 * 1000;
+        chrome.storage.local.set({
+            updateTime: _updateTime
+        });
+        chrome.storage.local.get(['updateTime'], function (result) {
+            chrome.alarms.create('updateCookieBase', { when: parseInt(result.updateTime) });
+        });
+    }
 });
 
 chrome.alarms.onAlarm.addListener(function (alarm) {
