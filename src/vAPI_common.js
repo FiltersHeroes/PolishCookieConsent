704c69dc94dbe50ae512ac91b6afca7fcbfebf05
hawkeye116477 <hawkeye116477@gmail.com>
2021-05-28 11:02:29 +0200

    Change comment sign in syntax conversion

--- src/vAPI_common.js
+++ src/vAPI_common.js
@@ -10,7 +10,7 @@ var PCC_vAPI_common = {
                         for (var i = 0; i < filters.length; i++) {
                             argsAfterFunc = filters[i].split("##+js(")[1];
                             if (typeof argsAfterFunc === "undefined") {
-                                filters[i] = filters[i].replace("(", "##+js(").replace("clickCompleteText", "clickComplete").split("|").join(",").replace(/^/, "!");
+                                filters[i] = filters[i].replace("(", "##+js(").replace("clickCompleteText", "clickComplete").split("|").join(",").replace(/^/, "#");
                             }
                         }
                         PCC_vAPI.storage.local.set("userFilters", filters.join("\n")).then(function () {
